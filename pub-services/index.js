ff199fb3acc24f427a71fbc2b86e856056afbbf1
Alex <alex.thepaut33@gmail.com>
2019-03-22 15:49:32 +0100

    Pub Service json to class

--- pub-services/index.js
+++ pub-services/index.js
@@ -1,7 +1,7 @@
-var services = require('./services/pubs.service');
+const pubServices = require('./services/pubs.service');
 
 module.exports = {
     services: {
-        pubServices: services
+        pubServices: pubServices
     }
 }
--- pub-services/services/pubs.service.js
+++ pub-services/services/pubs.service.js
@@ -1,10 +1,49 @@
-const pubs = require('../mocks/pubs');
+const pubs = require('../mocks/pubs.json');
+
+class Pubs {
+    constructor (name, owner , openDays, openHours, beers){
+        this.name = name;
+        this.owner = owner;
+        this.openDays = openDays;
+        this.openHours = openHours;
+        this.beers = beers;
+    }
+}
+
+class Owner {
+    constructor (firstName, lastName, mail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = mail;
+    }
+}
+
+class OpenHours{
+    constructor (start, end){
+        this.start = start;
+        this.end = end;
+    }
+}
+
+class Beers{
+    constructor (type, name){
+        this.type = type;
+        this.name = name;
+    }
+}
+
+let pub = pubs.map(b => new Pubs(b.name, 
+    new Owner(b.owner.firstName, b.owner.lastName, b.owner.mail), 
+    b.openDays, 
+    new OpenHours(b.openHours.start, b.openHours.end), 
+    new Beers(b.beers.type, b.beers.name)));
 
 function findAll(){
-    let coucou;
+    let coucou= '';
     for(let i = 0; i < pubs.length ; i ++){
-        coucou += `pubs ${i+1} : ${pubs[i].name}`;
+        coucou += `pub ${i+1} : ${pub[i].name} \n`;
     }
+    console.log(coucou);
     return coucou;
 }
 
@@ -13,6 +52,5 @@ function findOpenNow(){
 }
 
 module.exports = {
-    findAll: findAll,
-    findOpenNow: findOpenNow
+    findAll: findAll
 }
